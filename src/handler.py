b146bf947f07e36a68946f6f2176d4c48c814c4b
Witold Zolnowski <blue3656@gmail.com>
2023-11-01 08:56:09 +0000

    add missing comma to sentry init

--- src/handler.py
+++ src/handler.py
@@ -12,7 +12,7 @@ import requests
 import sentry_sdk
 
 sentry_sdk.init(
-    dsn = os.environ.get("SENTRY_DSN")
+    dsn = os.environ.get("SENTRY_DSN"),
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     traces_sample_rate=1.0,
